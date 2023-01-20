3083fd609c08f3ffb78bb11ab1329b3228f7e78a
Mohit <mohit.patel1966@gmail.com>
2023-01-20 14:01:10 +0530

    FCC test cases, 13 solved

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,21 @@ function App() {
 		const value = event.target.innerHTML
 		if(formula != "0"){
 			if(numbers.includes(lastDigit) || operators.includes(lastDigit)){
-				setOutputScreen(value)
+				if(outputScreen != "0"){
+					setOutputScreen(outputScreen + value)
+				}else{
+					setOutputScreen(value)
+				}
 				setFormula(formula + value)
 				setLastDigit(value)
 			}else if(lastDigit == "."){
 				setOutputScreen(outputScreen + value)
 				setFormula(formula + value)
+			}
+			else if(lastDigit == "="){
+				clearScreen()
+				setFormula(value)
+				setOutputScreen(value)
 			}
 		}
 	}
@@ -69,9 +78,10 @@ function App() {
 	}
 
 	const equal = () => {
-		const answer = eval(formula).toFixed(4)
+		const answer = eval(formula).toFixed(2)
 		setFormula(formula + " = " + answer)
-		setOutputScreen(answer)
+		setOutputScreen(Number(answer))
+		setLastDigit("=")
 	}
 
 	return (
@@ -80,26 +90,26 @@ function App() {
 				<div className="bg-gray-900">
 					<div className="flex flex-col">
 						<div className="text-end pt-1 pb-1 pr-2 pl-2 text-lg min-h-[50px]">{formula}</div>
-						<div className="text-end pt-1 pb-1 pr-2 pl-2 text-2xl">{outputScreen}</div>
+						<div className="text-end pt-1 pb-1 pr-2 pl-2 text-2xl" id="display">{outputScreen}</div>
 					</div>
 					<div className="grid grid-rows-[auto] grid-template-area gap-1 p-2 bg-gray-900">
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-red-600 ac" onClick={clearScreen}>AC</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-500 divide" onClick={operator}>/</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-500 multiply" onClick={operator}>*</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 seven" onClick={number}>7</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 eight " onClick={number}>8</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 nine " onClick={number}>9</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-500 minus " onClick={operator}>-</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 four " onClick={number}>4</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 five " onClick={number}>5</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 six " onClick={number}>6</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-500 plus " onClick={operator}>+</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 one" onClick={number}>1</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 two" onClick={number}>2</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 three" onClick={number}>3</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-blue-600 equal" onClick={equal}>=</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 zero" onClick={number}>0</button>
-						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 point" onClick={point}>.</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-red-600 ac" id="clear" onClick={clearScreen}>AC</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-500 divide" id="divide" onClick={operator}>/</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-500 multiply" id="multiply" onClick={operator}>*</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 seven" id="seven" onClick={number}>7</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 eight " id="eight" onClick={number}>8</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 nine " id="nine" onClick={number}>9</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-500 minus " id="subtract" onClick={operator}>-</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 four " id="four" onClick={number}>4</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 five " id="five" onClick={number}>5</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 six " id="six" onClick={number}>6</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-500 plus " id="add" onClick={operator}>+</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 one" id="one" onClick={number}>1</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 two" id="two" onClick={number}>2</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 three" id="three" onClick={number}>3</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-blue-600 equal" id="equals" onClick={equal}>=</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 zero" id="zero" onClick={number}>0</button>
+						<button className="text-center pl-8 pr-8 pt-4 pb-4 bg-gray-400 point" id="decimal" onClick={point}>.</button>
 					</div>
 				</div>
 			</div>
