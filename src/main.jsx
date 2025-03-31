fd47e0065216c5d897be704c0b49a201e0e3e9d7
smarty <mohit.patel1966@gmail.com>
2025-03-31 17:01:06 +0530

    react onesignal added

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-// Initialize OneSignal
-window.OneSignal = window.OneSignal || [];
-OneSignal.push(function() {
-  OneSignal.init({
-    appId: "76e82076-dd38-4313-a8f2-c828f8a71a73",
-    notifyButton: {
-      enable: true,
-    },
-  });
-});
+useEffect(() => {
+  // Ensure this code runs only on the client side
+  if (typeof window !== 'undefined') {
+    OneSignal.init({
+      appId: '76e82076-dd38-4313-a8f2-c828f8a71a73',
+      // You can add other initialization options here
+      notifyButton: {
+        enable: true,
+      },
+      // Uncomment the below line to run on localhost. See: https://documentation.onesignal.com/docs/local-testing
+      allowLocalhostAsSecureOrigin: true
+    });
+  }
+}, []);
 
 ReactDOM.render(
   <React.StrictMode>
